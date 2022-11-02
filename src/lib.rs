8ed2ff093f06f69fa9ea8b48d0fd20988837dc81
Raymond <20248577+javaarchive@users.noreply.github.com>
2022-11-02 21:39:14 +0000

    fix formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use libc::c_char;
 use libc::c_void;
 
+use crate::host::hosting::HOST;
+
 mod utils;
 mod host;
 mod hooks;
@@ -11,6 +13,8 @@ fn main() {
 
 redhook::hook! {
     unsafe fn premain_plugin() => premain_plugin_first {
-        println!("Premain starting. Please wait. ");
+        if HOST.config.debug_mode {
+            println!("Premain starting. Please wait. ");
+        }
     }
 }
--- src/utils/config.rs
+++ src/utils/config.rs
@@ -1,9 +1,45 @@
-use std::env;
+use std::{env, str::FromStr};
 
 pub struct Config{
     pub enable_x11: bool,
     pub enable_gl: bool,
     pub debug_mode: bool,
+    // windowing
+    pub window_width_override: Option<u32>,
+    pub window_height_override: Option<u32>,
+}
+
+fn get<T: FromStr>(key: &str, default: T) -> T{
+    match env::var(key){
+        Ok(val) => val.parse::<T>().unwrap_or(default),
+        Err(_) => default,
+    }
+}
+
+fn try_get<T: FromStr>(key: &str) -> Option<T>{
+    match env::var(key){
+        Ok(val) => {
+            match val.parse::<T>(){
+                Ok(val) => Some(val),
+                Err(_) => None,
+            }
+        },
+        Err(_) => None
+    }
+}
+
+fn intify(key: &str, default: i32) -> i32{
+    match env::var(key){
+        Ok(val) => val.parse::<i32>().unwrap_or(default),
+        Err(_) => default,
+    }
+}
+
+fn u_intify(key: &str, default: u32) -> u32{
+    match env::var(key){
+        Ok(val) => val.parse::<u32>().unwrap_or(default),
+        Err(_) => default,
+    }
 }
 
 fn booleanify(key: &str, default: bool) -> bool{
@@ -29,8 +65,10 @@ impl Config {
     pub fn from_env() -> Config {
         Config {
             enable_x11: booleanify("ENABLE_X11", true),
-            enable_gl: booleanify("ENABLE_X11", true),
+            enable_gl: booleanify("ENABLE_GL", true),
             debug_mode: booleanify("DEBUG_HW", false),
+            window_width_override: try_get::<u32>("WINDOW_WIDTH"),
+            window_height_override: try_get::<u32>("WINDOW_HEIGHT"),
         }
     }
 }
@@ -41,6 +79,8 @@ impl Default for Config{
             enable_x11: true,
             enable_gl: true,
             debug_mode: false,
+            window_width_override: None,
+            window_height_override: None,
         }
     }
 }
