048c9819b56be4567d18dcc5b5fe1a47beed9567
Raymond <20248577+javaarchive@users.noreply.github.com>
2022-11-09 21:18:46 +0000

    wait nvm

--- src/host/connection.rs
+++ src/host/connection.rs
@@ -1,6 +1,7 @@
 // use std::error::Error;
 use std::io::Error;
 use std::sync::mpsc::{self, channel, Receiver, Sender};
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 use super::message::Message;
@@ -16,7 +17,7 @@ pub trait Transport {
 }
 
 pub struct Connection {
-    pub transport: Box<dyn Transport + Send + Sync>,
+    pub transport: Arc<Mutex<Box<dyn Transport + Send + Sync>>>, // super nesting lol
     // message output queue
     outgoing: (Sender<Message>, Receiver<Message>),
     // message input queue
@@ -33,7 +34,7 @@ impl Connection {
         // let (tx1, rx1) = mpsc::channel();
         // let (tx2, rx2) = mpsc::channel();
         let conn = Connection {
-            transport: Box::new(transport),
+            transport: Arc::new(Box::new(transport)),
             outgoing: mpsc::channel(), // (tx1, rx1),
             ingoing: mpsc::channel(),  // (tx2, rx2),
         };
@@ -42,16 +43,22 @@ impl Connection {
     }
 
     pub fn start(&mut self) {
-        let transport = &mut self.transport;
-        transport.init().expect("Transport initalization failure. ");
+        let transport_arcmutex = &mut self.transport;
+        transport_arcmutex
+            .lock()
+            .unwrap()
+            .init()
+            .expect("Transport initalization failure. ");
         /* thread::spawn(move || {
             transport.init().expect("Transport initalization fail. ");
             // read/write queue here
         }); */
         // rw threads
         thread::spawn(move || {
+            let transport = &mut transport_arcmutex.lock().unwrap();
             // let tx_recieved = self.outgoing.0.clone();
             let buf = [0; MAX_PAYLOAD];
+            
             loop {
                 transport.recv(&mut buf).expect("Transport recv fail. ");
             }
