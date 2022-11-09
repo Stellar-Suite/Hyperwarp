bbd87c0b51655fc28b6409368cf58f1ee4efac57
Raymond <20248577+javaarchive@users.noreply.github.com>
2022-11-09 22:36:06 +0000

    end message

--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@ echo Runner: Bootstraping
 BASE=$(pwd)
 echo Runner: $(pwd) is our base path. 
 LD_PRELOAD="$BASE/target/debug/libhyperwarphooker.so $BASE/libhyperglue.so" $@
+echo Runner: Program ended
