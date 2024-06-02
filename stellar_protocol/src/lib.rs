301eb8fc98dafc0d269680cb208273522a76c5b8
Raymond <20248577+javaarchive@users.noreply.github.com>
2024-06-01 17:25:52 -0700

    impl basic internal protocol

--- stellar_protocol/src/lib.rs
+++ stellar_protocol/src/lib.rs
@@ -1,1 +1,18 @@
+use lazy_static::lazy_static;
+use bincode;
+use crate::protocol::StellarMessage;
+
 pub mod protocol;
+
+// for eventual bincode 2 migration
+// lazy_static! {
+    
+// }
+
+pub fn serialize(msg: StellarMessage) -> Vec<u8> {
+    bincode::serialize(&msg).unwrap()
+}
+
+pub fn deserialize(data: &[u8]) -> StellarMessage {
+    bincode::deserialize(data).unwrap()
+}
