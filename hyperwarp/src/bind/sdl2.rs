e50f3a254ff9faa807ae1dc3d6d5f2c52e325751
Raymond <20248577+javaarchive@users.noreply.github.com>
2024-07-03 19:55:59 -0700

    input basic + add sdl2-sys for structs

--- hyperwarp/src/bind/sdl2.rs
+++ hyperwarp/src/bind/sdl2.rs
@@ -12,5 +12,29 @@ lazy_static::lazy_static! {
         std::mem::transmute(ptr)
     };
 
-    
+
 }
+
+// portable key mods, from SDL
+// https://wiki.libsdl.org/SDL2/SDL_Keymod
+
+pub const KMOD_NONE: u32 = 0x0000;
+pub const KMOD_LSHIFT: u32 = 0x0001;
+pub const KMOD_RSHIFT: u32 = 0x0002;
+pub const KMOD_LCTRL: u32 = 0x0040;
+pub const KMOD_RCTRL: u32 = 0x0080;
+pub const KMOD_LALT: u32 = 0x0100;
+pub const KMOD_RALT: u32 = 0x0200;
+pub const KMOD_LGUI: u32 = 0x0400;
+pub const KMOD_RGUI: u32 = 0x0800;
+pub const KMOD_NUM: u32 = 0x1000;
+pub const KMOD_CAPS: u32 = 0x2000;
+pub const KMOD_MODE: u32 = 0x4000;
+pub const KMOD_SCROLL: u32 = 0x8000;
+
+pub const KMOD_CTRL: u32 = KMOD_LCTRL | KMOD_RCTRL;
+pub const KMOD_SHIFT: u32 = KMOD_LSHIFT | KMOD_RSHIFT;
+pub const KMOD_ALT: u32 = KMOD_LALT | KMOD_RALT;
+pub const KMOD_GUI: u32 = KMOD_LGUI | KMOD_RGUI;
+
+pub const KMOD_RESERVED: u32 = KMOD_SCROLL; /* "This is for source-level compatibility with SDL 2.0.0."" */
--- hyperwarp/src/host/input.rs
+++ hyperwarp/src/host/input.rs
@@ -1,13 +1,97 @@
+use std::collections::HashMap;
+
+use super::hosting::HOST;
+
+// abstraction for data
 pub struct Mouse {
-    
+    pub x: i32,
+    pub y: i32,
+}
+
+impl Mouse {
+    pub fn new() -> Mouse {
+        Mouse {
+            x: 0,
+            y: 0,
+        }
+    }
 }
 
 pub struct Keyboard {
+    pub state: HashMap<i32, bool>, // key code _> pressed
+}
 
+pub fn create_init_keyboard_state() -> HashMap<i32, bool> {
+    let mut state = HashMap::new();
+    for i in 0..256 {
+        state.insert(i, false);
+    }
+    state
+}
+
+impl Keyboard {
+    pub fn new() -> Keyboard {
+        Keyboard {
+            state: create_init_keyboard_state(),
+        }
+    }
+}
+
+pub struct InputEvent {
+    pub payload: InputEventPayload,
+}
+
+pub struct InputMetadata {
+    pub sdl2_timestamp_ticks: Option<u32>,
+    pub sdl2_timestamp_ticks_u64: Option<u64>,
+    pub sdl3_timestamp_ticks: Option<u32>,
+    pub sdl3_timestamp_ticks_u64: Option<u64>,
+}
+
+
+
+pub enum InputEventPayload {
+    MouseMoveRelative(i32, i32),
+    MouseMoveAbsolute(i32, i32),
 }
 
 pub struct InputManager {
     pub mouse: Mouse,
     pub keyboard: Keyboard,
     // TODO: vec of gamepads
+    pub event_queue: Vec<InputEvent>,
+}
+
+impl InputManager {
+    pub fn move_mouse_absolute(&mut self, x: i32, y: i32) {
+        
+    }
+
+    pub fn move_mouse_relative(&mut self, x: i32, y: i32) {
+        let mut final_x = self.mouse.x + x;
+        let mut final_y = self.mouse.y + y;
+        // clamp
+        if let Some((width, height)) = HOST.get_behavior().get_fb_size() {
+            final_x = final_x.clamp(0, width as i32);
+            final_y = final_y.clamp(0, height as i32);
+        }
+        if self.mouse.x == final_x && self.mouse.y == final_y {
+            return;
+        }
+        self.mouse.x = final_x;
+        self.mouse.y = final_y;
+        
+    }
+
+    pub fn set_key(&mut self, key: i32, state: bool) {
+        self.keyboard.state.insert(key, state);
+    }
+
+    pub fn new() -> InputManager {
+        InputManager {
+            mouse: Mouse::new(),
+            keyboard: Keyboard::new(),
+            event_queue: Vec::new(),
+        }
+    }
 }
